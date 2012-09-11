64fcc4a252f2afda42705e1e9c02fa1be0f2037d
Zac Meadows <zmeadows@gmail.com>
2012-09-11 18:30:50 -0500

    starting c solutions

---
+++ c/euler.h
@@ -0,0 +1,18 @@
+#ifndef EULER_H
+#define EULER_H
+
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+int* sieve(int n) {
+  int i,j;
+  int primes[n];
+  for (i=0;i<n;++i) {
+    printf("%i",primes[i]);
+  }
+  for (i=2;i<=sqrt(n);++i) {
+  }
+}
+
+#endif
